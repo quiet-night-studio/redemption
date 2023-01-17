89a5e79fdc71f97c554effc34ab45ddf824f3a7a
Gerep <daniel.gerep@skiff.com>
2023-01-17 09:16:50 -0300

    working on the aim issue #1

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 export (PackedScene) var Bullet
 
-const RADIUS = 50
+const RADIUS = 30
 
 var speed = 200
 var velocity = Vector2.ZERO
@@ -26,11 +26,11 @@ func get_input() -> void:
 
 func _physics_process(_delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
-	var player_pos = global_transform.origin 
-	var distance = player_pos.distance_to(mouse_pos) 
+	var player_pos = global_transform.origin
+	var distance = player_pos.distance_to(mouse_pos)
 	var mouse_dir = (mouse_pos-player_pos).normalized()
 
-	if distance > RADIUS:
+	if distance != RADIUS:
 		mouse_pos = player_pos + (mouse_dir * RADIUS)
 
 	$Muzzle.global_transform.origin = mouse_pos
@@ -41,14 +41,31 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide(velocity)
 
 func shoot() -> void:
+	var cursor_pos = get_global_mouse_position()
+	var distance = cursor_pos.distance_to(global_transform.origin)
+
 	var b = Bullet.instance()
+	
+	if distance <= RADIUS:
+		# Cursor is inside the circle
+		print("Cursor is inside the circle.")
+
+		var bullet_pos = global_transform.origin
+		var angle = atan2(cursor_pos.y - global_transform.origin.y, cursor_pos.x - global_transform.origin.x)
+
+	else:
+		# Cursor is outside the circle
+		b.transform = $Muzzle.global_transform
+
 	get_tree().get_root().add_child(b)
-	b.transform = $Muzzle.global_transform
 
 func kill() -> void:
 	var err = get_tree().reload_current_scene()
 	if err != OK:
 		print("error reloading scene: ", err)
 
-func _on_Area2D_area_entered(_area) -> void:
+func _on_Area2D_area_entered(area) -> void:
+	if area.is_in_group("bullet"):
+		return
+
 	kill()
