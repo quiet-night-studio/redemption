1074ee9176a11e22949d95a142e58c7dad29c789
gerep <daniel.gerep@skiff.com>
2023-02-15 08:27:00 -0300

    Add Path2D to spawn enemies

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-onready var mobs = get_node("Mobs")
+onready var _mobs = get_node("Mobs")
+onready var _mob_spawn_location := $MobPath/MobSpawnLocation
 
 var mob: PackedScene = preload("res://scenes/Mob.tscn")
 var spawn_time = 0
@@ -8,7 +9,7 @@ var spawn_time = 0
 func _ready():
 	randomize()
 	spawn_time = randi() % 5 + 1
-	
+
 	var err = $Player.connect("update_health", $CanvasLayer/HealthDisplay, "update_healthbar")
 	if err != OK:
 		print("error connecting signal update_health: ", err)
@@ -21,6 +22,9 @@ func _process(delta):
 		spawn_time = randi() % 5 + 1
 
 func spawn():
+	_mob_spawn_location.unit_offset = randf()
+
 	var m = mob.instance()
-	m.position = Vector2(randi() % 1024, randi() % 600)
-	mobs.add_child(m)
+	_mobs.add_child(m)
+
+	m.position = _mob_spawn_location.position
