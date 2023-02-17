6132fbb00c12094ca53e688988cb235ea4dda6a5
gerep <daniel.gerep@skiff.com>
2023-02-17 09:28:06 -0300

    Remove annoying _ from code

--- scripts/Mob.gd
+++ scripts/Mob.gd
@@ -1,39 +1,39 @@
 extends KinematicBody2D
 
-var _speed := 100.0
-var _player := KinematicBody2D
-var _velocity := Vector2.ZERO
+var speed := 100.0
+var player := KinematicBody2D
+var velocity := Vector2.ZERO
 
-onready var _agent := $NavigationAgent2D
-onready var _timer := $Timer
-onready var _sprite := $Sprite
+onready var agent := $NavigationAgent2D
+onready var timer := $Timer
+onready var sprite := $Sprite
 
 func _ready() -> void:
-	_timer.connect("timeout", self, "_update_pathfinding")
+	timer.connect("timeout", self, "update_pathfinding")
 	
-	_player = get_tree().get_nodes_in_group("player")[0]
+	player = get_tree().get_nodes_in_group("player")[0]
 
 	$Sprite.modulate = Color(randf(), randf(), randf())
 
-	_agent.set_navigation(get_tree().get_root().get_node("Level").get_node("Navigation2D"))
-	_update_pathfinding()
+	agent.set_navigation(get_tree().get_root().get_node("Level").get_node("Navigation2D"))
+	update_pathfinding()
 
 func _physics_process(delta: float) -> void:
-	if _agent.is_navigation_finished():
+	if agent.is_navigation_finished():
 		return
 
-	var direction := global_position.direction_to(_agent.get_next_location())
+	var direction := global_position.direction_to(agent.get_next_location())
 
-	var desired_velocity := direction * _speed
-	var steering := (desired_velocity - _velocity) * delta * 4.0
-	_velocity += steering
+	var desired_velocity := direction * speed
+	var steering := (desired_velocity - velocity) * delta * 4.0
+	velocity += steering
 
-	_sprite.rotation = _velocity.angle()
+	sprite.rotation = velocity.angle()
 
-	_velocity = move_and_slide(_velocity)
+	velocity = move_and_slide(velocity)
 
-func _update_pathfinding() -> void:
-	_agent.set_target_location(_player.global_position)
+func update_pathfinding() -> void:
+	agent.set_target_location(player.global_position)
 
 func _on_HazzardArea_body_entered(body: Node) -> void:
 	queue_free()
