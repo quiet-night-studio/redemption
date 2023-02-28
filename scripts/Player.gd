19a05a2144e634c22edd3a605bf3f0994e7ba339
gerep <daniel.gerep@skiff.com>
2023-02-28 07:43:06 -0300

    add reloading

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,18 +1,33 @@
 extends KinematicBody2D
 
-var max_health = 100
-var speed = 200
-var bullet: PackedScene = preload("res://scenes/Bullet.tscn")
-
 signal update_health
 
-func _physics_process(_delta: float) -> void:
-	var mouse_world = get_global_mouse_position()
-	var player_world = global_position
+enum State { NORMAL, RELOADING }
 
-	var facing_direction = mouse_world - player_world
-	var muzzle_position = facing_direction.normalized() * Vector2(50, 50)
+onready var reload_timer := $ReloadTimer
+onready var info_label := $Info
 
+var bullet: PackedScene = preload("res://scenes/Bullet.tscn")
+
+var max_health := 100
+var speed := 200
+var magazine_size := 10
+var reload_interval := 3
+var current_bullets := 0
+var shooting := false
+var reloading := false
+var current_state = State.NORMAL
+
+func _ready() -> void:
+	reload_timer.connect("timeout", self, "_on_reaload_timer_timeout")
+	reload_timer.wait_time = reload_interval
+
+# Timeout for how long it takes to reload.
+func _on_reaload_timer_timeout() -> void:
+	info_label.text = ""
+	current_state = State.NORMAL
+
+func get_movement() -> void:
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
@@ -29,7 +44,28 @@ func _physics_process(_delta: float) -> void:
 	# warning-ignore:return_value_discarded
 	move_and_slide(velocity)
 
+func _physics_process(delta: float) -> void:
+	get_movement()
+
 	if Input.is_action_just_pressed("shoot"):
+		if current_state == State.RELOADING:
+			info_label.text = "still reloading!!!"
+			return
+
+		current_bullets += 1
+		if current_bullets >= magazine_size:
+			info_label.text = "reloading..."
+			current_bullets = 0
+			current_state = State.RELOADING
+			reload_timer.start()
+			return
+
+		var mouse_world = get_global_mouse_position()
+		var player_world = global_position
+
+		var facing_direction = mouse_world - player_world
+		var muzzle_position = facing_direction.normalized() * Vector2(35, 35)
+
 		var b = bullet.instance() as RigidBody2D
 		b.global_position = global_position + muzzle_position
 		b.set_velocity(facing_direction.normalized())
