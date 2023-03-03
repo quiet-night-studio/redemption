4037b594f5a43a3ad9806875ffd10bc443d05b30
Extremos <96099928+ProjectEXT@users.noreply.github.com>
2023-03-03 16:55:24 -0500

    Enabled One Shot for ReloadTimer
    Previously, after reloading, the ammo count would reset to 10 bullets after every shot was taken because the Reload Timer was replaying. This toggles One Shot to have it react only once and when called upon.
    
    Also renamed the label for Ammo Counter and moved it to a better position on the screen.

--- scripts/HealthDisplay.gd
+++ scripts/HealthDisplay.gd
@@ -6,4 +6,4 @@ func update_healthbar(value):
 	healthbar.value -= value
 	
 func _process(delta):
-	$Label.text = "Ammo: " + str(GameManager.current_bullets)
+	$AmmoCount.text = "Ammo: " + str(GameManager.current_bullets)
