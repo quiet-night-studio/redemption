89a58f9f9bb65489bbbda31ac9996c7e06b90e12
Extremos <96099928+ProjectEXT@users.noreply.github.com>
2023-03-10 17:49:45 -0500

    New cursor types
    Experimenting with basic cursor types. Made the commit to remove the older placeholder as it was funky.

--- scripts/mouse_cursor.gd
+++ scripts/mouse_cursor.gd
@@ -1,7 +1,7 @@
 extends Node
 
 # Temporary Cursor Image
-var cross_cursor = load("res://assets/cross-cursor.png")
+var cross_cursor = load("res://assets/cross_cursor.png")
 
 func _ready():
 	Input.set_custom_mouse_cursor(cross_cursor, Input.CURSOR_ARROW, Vector2(25,25))
