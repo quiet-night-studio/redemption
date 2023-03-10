dbf9280574b1e6e54182f2db9ba36e225f0e2777
Extremos <96099928+ProjectEXT@users.noreply.github.com>
2023-03-09 23:04:16 -0500

    Mouse Icon (Cross Cursor)
    Adds code to use a custom cursor for issue #23 .
    
    Resources:
    How-to - https://www.youtube.com/watch?v=5J470igeKkg&ab_channel=Kron
    
    Placeholder cursor - https://icons8.com/icon/113079/filled-circle

---
+++ scripts/mouse_cursor.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# Temporary Cursor Image
+var cross_cursor = load("res://assets/cross-cursor.png")
+
+func _ready():
+	Input.set_custom_mouse_cursor(cross_cursor, Input.CURSOR_ARROW, Vector2(25,25))
+#								   Var Name,      Default Cursor,  Hotspot/area clicked
